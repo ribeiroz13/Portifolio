3ca1feb78319f1497f1c52e9d129cf6580952159
jeferson ribeiro <128777436+ribeiroz13@users.noreply.github.com>
2024-09-09 00:36:58 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -10,3 +10,28 @@
     objetivo2 - esconder o botão de mostrar mais
         Passo 1 - pegar o botão e esconder ele
 */
+
+//Objetivo 1 - quando o usuário clicar no botão de mostra mais deve abrir os projetos que estão escondidos no html
+
+//Passo 1 - pegar o botão mostrar mais no JS pra poder verificar quando o usuário clicar em cima dele
+
+const botaoMostrarProjetos = document.querySelector('.btn-mostrar-projetos');
+const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)');
+
+botaoMostrarProjetos.addEventListener('click', () => {
+    
+    mostrarMaisProjetos();
+
+    esconderBotao();
+});
+
+function esconderBotao() {
+    botaoMostrarProjetos.classList.add("remover");
+}
+
+function mostrarMaisProjetos() {
+    projetosInativos.forEach((projetoInativo) => {
+        projetoInativo.classList.add('ativo');
+    });
+}
+
